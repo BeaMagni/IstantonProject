f5bb61819dd2e2ccef93936d3acdcaa15aaa8827
pietroghedini <138769547+pietroghedini@users.noreply.github.com>
2024-01-13 10:55:32 +0100

    Update Free_energy_as.py

--- Free_energy_as.py
+++ Free_energy_as.py
@@ -3,15 +3,15 @@ from numba import njit
 from scipy.integrate import simps
 import General_functions as fn
 
-etha = 1.4
-w0 = 5.6
-a = 0.05
+etha = float(input("Insert the value for etha, the shift of the potential: "))
+w0 = 4*etha
+a = float(input("Insert the value for a, the lattice spacing: "))
 delta_x = 0.5
-n_beta = 5
-beta_max = 40.0
-n_equil = 100
-n_sweeps = 100000
-n_switching = 20
+n_equil = int(input("Insert the number of Monte Carlo equilibration sweeps: "))
+n_sweeps = int(input("Insert the number of Monte Carlo sweeps (must be bigger than the previous value): "))
+n_switching = int(input("Insert the number of adiabatic switching sweeps: "))
+n_beta = int(input("Insert the number of steps for beta: "))
+beta_max = float(input("Insert the maximum value for beta: "))
 beta = np.linspace(1.0,beta_max,n_beta)
 temperature = 1.0/beta
 F = np.zeros(n_beta)
