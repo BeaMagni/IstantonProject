383fe69cc8948c65f153905c5e8cc0a6de14faa1
pietroghedini <138769547+pietroghedini@users.noreply.github.com>
2024-01-14 09:21:23 +0100

    Update MC_cooling.py

--- MC_cooling.py
+++ MC_cooling.py
@@ -32,6 +32,8 @@ def main():
     der_log_x3_err = np.zeros(29)
 
     start = bool(input("Insert the desired start (0-cold, 1-hot): "))
+    output_path = './instanton_project/cooling'
+    fn.path_creation(output_path)
     
     x = fn.initialize_lattice(N,etha,start)
     
@@ -40,13 +42,13 @@ def main():
     for j in range(n_sweeps-n_equil):
         x = fn.metropolis(x,a,delta_x,etha)
         if j%int((n_sweeps-n_equil)/2) == 0:
-            np.savetxt("C:/Users/115271/Desktop/UniBO/Theoretical and Numerical Aspects of Nuclear Physics/esame/cooling/montecarlo.txt", x)
+            np.savetxt(output_path + "/montecarlo.txt", x)
         if j%10 == 0:
             x_cool = np.copy(x)
             for _ in range(n_cooling_sweeps):
                 x_cool = fn.metropolis_cooling(x_cool,a,delta_x,etha)
             if j%int((n_sweeps-n_equil)/2) == 0:
-                np.savetxt("C:/Users/115271/Desktop/UniBO/Theoretical and Numerical Aspects of Nuclear Physics/esame/cooling/cooledmontecarlo.txt",x_cool)
+                np.savetxt(output_path + "/cooledmontecarlo.txt",x_cool)
             for _ in range(5):
                 count += 1
                 p0 = int((N-30)*np.random.uniform(0.0,1.0))
@@ -59,25 +61,23 @@ def main():
                     x2_cor_sum2[p] += np.power(x_cor,4)
                     x3_cor_sum2[p] += np.power(x_cor,6)
     
-    x_cor_av, x_cor_err = fn.average_std(x_cor_sum,x_cor_sum2,count)
+    x1_cor_av, x1_cor_err = fn.average_std(x_cor_sum,x_cor_sum2,count)
     x2_cor_av, x2_cor_err = fn.average_std(x2_cor_sum,x2_cor_sum2,count)
     x3_cor_av, x3_cor_err = fn.average_std(x3_cor_sum,x3_cor_sum2,count)
-    der_log_x, der_log_x_err = fn.derivative_log(x_cor_av,x_cor_err,a)
+    der_log_x1, der_log_x1_err = fn.derivative_log(x_cor_av,x_cor_err,a)
     x2_cor_av_sub = x2_cor_av-x2_cor_av[-1]
     x2_cor_err_sub = np.sqrt(np.power(x2_cor_err,2)+np.power(x2_cor_err[-1],2))
     der_log_x2, der_log_x2_err = fn.derivative_log(x2_cor_av_sub,x2_cor_err,a)
     der_log_x3, der_log_x3_err = fn.derivative_log(x3_cor_av,x3_cor_err,a)
 
-    output_path = './instanton_project/cooling'
-    fn.path_creation(output_path)
-    np.savetxt(output_path + '/x_cor_av.txt',x_cor_av)
+    np.savetxt(output_path + '/x1_cor_av.txt',x1_cor_av)
     np.savetxt(output_path + '/x2_cor_av.txt',x2_cor_av)
     np.savetxt(output_path + '/x3_cor_av.txt',x3_cor_av)
-    np.savetxt(output_path + '/x_cor_err.txt',x_cor_err)
+    np.savetxt(output_path + '/x1_cor_err.txt',x1_cor_err)
     np.savetxt(output_path + '/x2_cor_err.txt',x2_cor_err)
     np.savetxt(output_path + '/x3_cor_err.txt',x3_cor_err)
-    np.savetxt(output_path + '/der_log_x.txt',der_log_x)
-    np.savetxt(output_path + '/der_log_x_err.txt',der_log_x_err)
+    np.savetxt(output_path + '/der_log_x1.txt',der_log_x1)
+    np.savetxt(output_path + '/der_log_x1_err.txt',der_log_x1_err)
     np.savetxt(output_path + '/der_log_x2.txt',der_log_x2)
     np.savetxt(output_path + '/der_log_x2_err.txt',der_log_x2_err)
     np.savetxt(output_path + '/der_log_x3.txt',der_log_x3)
