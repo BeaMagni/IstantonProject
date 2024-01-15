142bc6a04a9f24db6c4e9fdac53984d1346e9733
pietroghedini <138769547+pietroghedini@users.noreply.github.com>
2024-01-15 11:24:06 +0100

    Update MC_cooling.py

--- MC_cooling.py
+++ MC_cooling.py
@@ -1,6 +1,8 @@
 import numpy as np
-from numba import njit
 import General_functions as fn
+
+#monte carlo simulation using the cooling method: computation of correlation functions and their logarithmic derivatives and euclidean path
+#the parameters are left as an input from keyboard
 
 def main():
     N = int(input("Insert N, the dimension of the lattice: "))
@@ -35,23 +37,24 @@ def main():
     output_path = './instanton_project/cooling'
     fn.path_creation(output_path)
     
-    x = fn.initialize_lattice(N,etha,start)
+    x = fn.initialize_lattice(N, etha, start)
     
-    for _ in range(n_equil):
-        x = fn.metropolis(x,a,delta_x,etha)
+    for _ in range(n_equil): #equilibration runs for the metropolis algorithm
+        x = fn.metropolis(x, a, delta_x, etha)
+    
     for j in range(n_sweeps-n_equil):
-        x = fn.metropolis(x,a,delta_x,etha)
-        if j%int((n_sweeps-n_equil)/2) == 0:
+        x = fn.metropolis(x, a, delta_x, etha)
+        if j%int((n_sweeps-n_equil)/2) == 0: #we only save some of the configurations
             np.savetxt(output_path + "/montecarlo.txt", x)
-        if j%10 == 0:
+        if j%10 == 0: #we don't do the cooling for all the metropolis sweeps
             x_cool = np.copy(x)
             for _ in range(n_cooling_sweeps):
-                x_cool = fn.metropolis_cooling(x_cool,a,delta_x,etha)
-            if j%int((n_sweeps-n_equil)/2) == 0:
+                x_cool = fn.metropolis_cooling(x_cool, a, delta_x, etha)
+            if j%int((n_sweeps-n_equil)/2) == 0: #we only save some of the configurations
                 np.savetxt(output_path + "/cooledmontecarlo.txt",x_cool)
             for _ in range(5):
                 count += 1
-                p0 = int((N-30)*np.random.uniform(0.0,1.0))
+                p0 = int((N-30)*np.random.uniform(0.0,1.0)) #we consider a rescaled (based on the total number of lattice points) random number
                 for p in range(30):
                     x_cor = x_cool[p0]*x_cool[p0+p]
                     x_cor_sum[p] += x_cor
@@ -61,14 +64,14 @@ def main():
                     x2_cor_sum2[p] += np.power(x_cor,4)
                     x3_cor_sum2[p] += np.power(x_cor,6)
     
-    x1_cor_av, x1_cor_err = fn.average_std(x_cor_sum,x_cor_sum2,count)
-    x2_cor_av, x2_cor_err = fn.average_std(x2_cor_sum,x2_cor_sum2,count)
-    x3_cor_av, x3_cor_err = fn.average_std(x3_cor_sum,x3_cor_sum2,count)
-    der_log_x1, der_log_x1_err = fn.derivative_log(x_cor_av,x_cor_err,a)
+    x1_cor_av, x1_cor_err = fn.average_std(x_cor_sum, x_cor_sum2, count)
+    x2_cor_av, x2_cor_err = fn.average_std(x2_cor_sum, x2_cor_sum2, count)
+    x3_cor_av, x3_cor_err = fn.average_std(x3_cor_sum, x3_cor_sum2, count)
+    der_log_x1, der_log_x1_err = fn.derivative_log(x_cor_av, x_cor_err, a)
     x2_cor_av_sub = x2_cor_av-x2_cor_av[-1]
     x2_cor_err_sub = np.sqrt(np.power(x2_cor_err,2)+np.power(x2_cor_err[-1],2))
-    der_log_x2, der_log_x2_err = fn.derivative_log(x2_cor_av_sub,x2_cor_err,a)
-    der_log_x3, der_log_x3_err = fn.derivative_log(x3_cor_av,x3_cor_err,a)
+    der_log_x2, der_log_x2_err = fn.derivative_log(x2_cor_av_sub, x2_cor_err, a)
+    der_log_x3, der_log_x3_err = fn.derivative_log(x3_cor_av, x3_cor_err, a)
 
     np.savetxt(output_path + '/x1_cor_av.txt',x1_cor_av)
     np.savetxt(output_path + '/x2_cor_av.txt',x2_cor_av)
